ec58a570e6e30a379ad3f55ad83f8f782314e1de
Ben Flores <benjaminhyw@gmail.com>
2017-09-19 14:20:34 -0700

    Create is-palindrome.js, add rules and pseudocode

---
+++ is-palindrome.js
@@ -0,0 +1,14 @@
+// Is Palindrome Challenge
+
+/*Rules:
+  Take any string as an argument
+    Return true if palindrome
+    return false if not
+*/
+
+/*PSEUDOCODE
+  1) Split string up to individual characters, strip away punctuation
+  2) Create new array with reversed array from original
+  3) Compare the two, and return true or false
+*/
+
