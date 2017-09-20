0acb4784fb962a7fedfcf36776fc1f94c5a9fc59
Ben Flores <benjaminhyw@gmail.com>
2017-09-19 18:46:37 -0700

    Add fully functional refactored course answer

--- caesar-cipher.js
+++ caesar-cipher.js
@@ -23,6 +23,8 @@
   5) Return new string
 */
 
+// NOTE: The negative number portion to this first answer isn't fully functional
+
 // function caesarCipher(str, num){
 //   str = str.toLowerCase();
 //   var strArr = str.split("");
@@ -52,3 +54,29 @@
 //   });
 //   return newArr.join("");
 // }
+
+function caesarCipher(str,num) {
+  num = num % 26;
+  var lowerCaseString = str.toLowerCase();
+  var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  var newString = '';
+
+  for (var i = 0; i < lowerCaseString.length; i++) {
+    var currentLetter = lowerCaseString[i];
+    if (currentLetter === ' ') {
+      newString += currentLetter;
+      continue;
+    }
+    var currentIndex = alphabet.indexOf(currentLetter);
+    var newIndex = currentIndex + num;
+    if (newIndex > 25) newIndex = newIndex - 26;
+    if (newIndex < 0) newIndex = 26 + newIndex;
+    if (str[i] === str[i].toUpperCase()) {
+      newString += alphabet[newIndex].toUpperCase();
+    }
+    else newString += alphabet[newIndex];
+  };
+
+  return newString;
+}
+caesarCipher('Zoo Keeper', 2);
