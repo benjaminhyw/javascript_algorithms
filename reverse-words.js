fb173a16d149c46473bf0b97c75a522d191213ab
Ben Flores <benjaminhyw@gmail.com>
2017-09-20 12:59:47 -0700

    Include refactored class answer

--- reverse-words.js
+++ reverse-words.js
@@ -42,4 +42,17 @@
 //   return emptyStrArr.join(' ');
 // }
 
+function reverseWords(string) {
+  var wordsArr = string.split(' ');
+  var reversedWordsArr = [];
 
+  wordsArr.forEach(word => {
+    var reversedWord = '';
+    for (var i = word.length - 1; i >= 0; i--) {
+      reversedWord += word[i];
+    };
+    reversedWordsArr.push(reversedWord);
+  });
+
+  return reversedWordsArr.join(' ');
+}
