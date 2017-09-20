0aab93b675fa0d4b3e81c76ef912206ab6754dbe
Ben Flores <benjaminhyw@gmail.com>
2017-09-20 13:28:54 -0700

    Create reverse-array-in-place.js, add rules and pseudocode

---
+++ reverse-array-in-place.js
@@ -0,0 +1,21 @@
+// Reverse Array In Place
+
+/*RULES:
+  Take array as parameter
+    Reverse array
+    Return reversed array
+
+  NOTES:
+  Can't create new empty array to push in
+  Must modify given array
+  Can't use array.reverse();
+*/
+
+/*PSEUDOCODE
+
+  1) Find array length
+  2) Until last element is first element, 
+    2a) Push first element to end of array
+    2b) Splice first element out of there
+  3) When done, return array
+*/
