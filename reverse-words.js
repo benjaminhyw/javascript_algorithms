12cf9f03f68b536e20c675444243722db1183422
Ben Flores <benjaminhyw@gmail.com>
2017-09-20 12:33:32 -0700

    Create reverse-words.js, add rules and pseudocode

---
+++ reverse-words.js
@@ -0,0 +1,23 @@
+// Reverse Words
+
+/*RULES:
+  Function takes a string parameter
+    Reverse every word in the string
+    Return new string
+
+  NOTE:
+    Order of words shouldn't change
+    Can't use the array.reverse() method
+*/
+
+/*PSEUDOCODE
+
+  1) Split array by words
+  2) For each word,
+    2a) Push last letter of each individual element into a new array
+    2b) When done, push new array into new empty str array
+    2c) Continue doing this for all words
+  3) Join new array
+  4) Return new string
+*/
+
