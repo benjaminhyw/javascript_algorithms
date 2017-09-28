90717c406a36ffe6b5bad87af66710ace1939110
Ben Flores <benjaminhyw@gmail.com>
2017-09-27 18:46:29 -0700

    Add memoized fibonacci solution

--- memoized-fibonacci.js
+++ memoized-fibonacci.js
@@ -15,3 +15,15 @@
     If number is not in cache, calculate it and put it in cache so it can be used multiple times in future
     O(n) runtime complexity
 */
+
+function fibMemo(index, cache) {
+  cache = cache || [];
+  if (cache[index]) return cache[index];
+  else {
+    if (index < 3) return 1;
+    else {
+      cache[index] = fibMemo(index - 1, cache) + fibMemo(index - 2, cache);
+    }
+  }
+  return cache[index];
+}
