3df6095135dc3f87a33624c8d8d09b0d022ab786
Ben Flores <benjaminhyw@gmail.com>
2017-09-29 16:01:33 -0700

    Create merge-sort.js, add rules and pseudocode

---
+++ merge-sort.js
@@ -0,0 +1,25 @@
+// Merge Sort
+
+/*RULES:
+  Two function algorithm
+  function mergeSort(arr){
+    // take in a single, unsorted array as a parameter
+    // split the array into two halves
+  }
+
+  function merge (array1, array2){
+    // takes in two sorted arrays as parameters
+    // merges those sorted arrays into one sorted array
+    // returns one sorted array
+  }
+*/
+
+/*PSEUDOCODE
+  1) Check array length
+  2) If array.length > 1
+    2a) split up array using mergeSort function
+  3) Start merging arrays, but only into arrays of two elements at first
+  4) Continue doing this until you're left with two main arrays
+  5) Final merge 
+  6) Return final array
+*/
